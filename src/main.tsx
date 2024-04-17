155d38eee84689c3d65d402efa7956076db80988
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-04-17 13:27:19 -0400

    refactor(src): update styles implementation for modern css and sass/scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './assets/styles/style.global.scss';
+import './styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
