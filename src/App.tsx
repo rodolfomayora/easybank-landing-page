851d772fb3651a0ec15867c60e4d87da0adb69a3
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-04-17 22:19:45 -0400

    refactor(root): revome initial loader (splash screen)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// import { useEffect } from 'react';
 import { Layout } from '#components/Layout';
 import { AtributionBlock } from '#components/AtributionBlock'
 
@@ -7,13 +6,6 @@ import { FeatureSection } from './landingSections/FeatureSection';
 import { MainSection } from './landingSections/MainSection';
 
 export function App () {
-
-  // useEffect(() => {
-  //   const loader = document.getElementById('loader');
-  //   loader?.classList.add("offLoader");
-  //   setTimeout(() => loader?.remove(), 1000);
-  // }, [])
-
   return (
     <>
       <Layout>
