e42f1ad0e995bd584651ae70593494da4ad141da
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-04-20 11:50:30 -0400

    refactor(pages): change name of page and location of section components

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,16 @@
+// import { Layout } from '#components/Layout';
+import { BasicLayout } from "#layouts/BasicLayout";
+// import { ArticleSection } from './ArticleSection';
+// import { FeatureSection } from './FeatureSection';
+// import { MainSection } from './MainSection';
+
+export function HomePage () {
+  return (
+    <BasicLayout>
+      ddd
+      {/* <MainSection /> */}
+      {/* <FeatureSection /> */}
+      {/* <ArticleSection /> */}
+    </BasicLayout>
+  );
+}
