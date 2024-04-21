0120fafe0768327793447bc4c08426799bae19a3
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-04-21 12:15:35 -0400

    refactor(pages): update home page main section  implementation

--- src/pages/HomePage/MainSection/index.tsx
+++ src/pages/HomePage/MainSection/index.tsx
@@ -1,34 +1,31 @@
 import { Container } from '#components/Container';
 import { ButtonInvitation } from '#components/ButtonInvitation';
 import { resolvePublicPath } from '#utils/resolvePublicPath';
+import { IntroDesktop } from '#assets/icons/svg';
 import styles from './styles.module.scss';
 
 export function MainSection () {
   const mockupSource = resolvePublicPath('/img/static/image-mockups.webp');
   return (
     <section className={styles.MainSection}>
-      <div className={styles.imageBase}>
-        <div className={styles.imageLayer}>
-          <div className={styles.imageBackground}></div>
-          <img className={styles.mockups}
-            src={mockupSource}
-            alt="App Mockup"
-          />
-        </div>
+      <div className={styles.imageBlock}>
+        <img className={styles.mockupsMobile}
+          src={mockupSource}
+          alt="App Mokups"
+        />
       </div>
-      
       <Container>
         <div className={styles.introInformation}>
           <h1 className={styles.mainTitle}>Next generation digital banking</h1>
-          <p className={styles.information}>Take your financial life online. Your Easybank account will be a one-top-shop for spending, saving, budgeting, investing, and much more</p>
+          <p className={styles.information}>Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more</p>
           <ButtonInvitation />
+          <IntroDesktop className={styles.imageBlockBackground} />
+          <img className={styles.mockupsDesktop}
+            src={mockupSource}
+            alt="App Mokups"
+          />
         </div>
       </Container>
-  
-      <img className={styles.mockupsDesktop}
-        src={mockupSource}
-        alt="App Mockup"
-      />
     </section>
   );
 }
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,14 +1,12 @@
-// import { Layout } from '#components/Layout';
 import { BasicLayout } from "#layouts/BasicLayout";
 // import { ArticleSection } from './ArticleSection';
 // import { FeatureSection } from './FeatureSection';
-// import { MainSection } from './MainSection';
+import { MainSection } from './MainSection';
 
 export function HomePage () {
   return (
     <BasicLayout>
-      ddd
-      {/* <MainSection /> */}
+      <MainSection />
       {/* <FeatureSection /> */}
       {/* <ArticleSection /> */}
     </BasicLayout>
