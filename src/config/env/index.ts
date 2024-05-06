f1224715d2afa297515116b8674b7cf1636cf2f8
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-05-06 15:05:17 -0400

    fix(src): solve router root path issue for docker deployments

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,7 +1,10 @@
+const isDevelopment = import.meta.env.DEV;
+
 const { BASE_URL } = import.meta.env;
 const isRoot = BASE_URL === '/';
 const basePublicPath = isRoot ? '' : BASE_URL;
 
 export {
   basePublicPath,
+  isDevelopment,
 }
