a18762bed43e1e3de578d16cdef3ded88ac8195d
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-05-24 08:36:36 -0400

    test(components): fix test running twice issue

--- src/components/Footer/FooterNavigation/index.test.tsx
+++ src/components/Footer/FooterNavigation/index.test.tsx
@@ -2,10 +2,9 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { FooterNavigation } from '.';
 
-
-describe('Component: FooterNavigation', () => {
+describe('Component: FoooterNavigation', () => {
   test('Should renders required links', () => {
-    render(<FooterNavigation />, { wrapper: BrowserRouter });
+  render(<FooterNavigation />, { wrapper: BrowserRouter });
 
     expect(screen.getByRole('link', { name: /^about us$/i })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /^contact$/i })).toBeInTheDocument();
@@ -13,5 +12,15 @@ describe('Component: FooterNavigation', () => {
     expect(screen.getByRole('link', { name: /^careers$/i })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /^support$/i })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /^privacy policy$/i })).toBeInTheDocument();
+  });
+
+  test('Should renders only internal navigation links', () => {
+    render(<FooterNavigation />, { wrapper: BrowserRouter });
+    const links = screen.getAllByRole('link');
+    links.forEach((link) => {
+      expect(link).toHaveAttribute('href');
+      expect(link).not.toHaveAttribute('target');
+      expect(link).not.toHaveAttribute('rel');
+    });
   });
 });
--- src/components/Footer/FooterNavigation/index.tsx
+++ src/components/Footer/FooterNavigation/index.tsx
@@ -3,14 +3,15 @@ import { resolvePath } from '#utils/resolvePath';
 import styles from './styles.module.scss';
 
 export function FooterNavigation () {
+  const pathToHome = resolvePath('/');
   return (
     <ul className={styles.FooterNavigation}>
-      <li><Link className={styles.navLink} to={resolvePath("/")}>About Us</Link></li>
-      <li><Link className={styles.navLink} to={resolvePath("/")}>Contact</Link></li>
-      <li><Link className={styles.navLink} to={resolvePath("/")}>Blog</Link></li>
-      <li><Link className={styles.navLink} to={resolvePath("/")}>Careers</Link></li>
-      <li><Link className={styles.navLink} to={resolvePath("/")}>Support</Link></li>
-      <li><Link className={styles.navLink} to={resolvePath("/")}>Privacy Policy</Link></li>
+      <li><Link className={styles.navLink} to={pathToHome}>About Us</Link></li>
+      <li><Link className={styles.navLink} to={pathToHome}>Contact</Link></li>
+      <li><Link className={styles.navLink} to={pathToHome}>Blog</Link></li>
+      <li><Link className={styles.navLink} to={pathToHome}>Careers</Link></li>
+      <li><Link className={styles.navLink} to={pathToHome}>Support</Link></li>
+      <li><Link className={styles.navLink} to={pathToHome}>Privacy Policy</Link></li>
     </ul>
   );
 }
